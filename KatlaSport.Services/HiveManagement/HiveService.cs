fb318cf98502eb2d7c4424a9b889daeb5bcdc517
Sergey Danilovich <danilovichser@gmail.com>
2018-06-21 22:25:00 +0300

    Merge step11. Created methods Add & Update & Delete hives in HiveController. Changed HiveManagementMappingProfile

--- KatlaSport.Services/HiveManagement/HiveService.cs
+++ KatlaSport.Services/HiveManagement/HiveService.cs
@@ -29,7 +29,7 @@ namespace KatlaSport.Services.HiveManagement
         }
 
         /// <inheritdoc/>
-        public async Task<List<HiveListItem>> GetHives()
+        public async Task<List<HiveListItem>> GetHivesAsync()
         {
             var dbHives = await _context.Hives.OrderBy(h => h.Id).ToArrayAsync();
             var hives = dbHives.Select(h => Mapper.Map<HiveListItem>(h)).ToList();
@@ -43,7 +43,7 @@ namespace KatlaSport.Services.HiveManagement
         }
 
         /// <inheritdoc/>
-        public async Task<Hive> GetHive(int hiveId)
+        public async Task<Hive> GetHiveAsync(int hiveId)
         {
             var dbHives = await _context.Hives.Where(h => h.Id == hiveId).ToArrayAsync();
             if (dbHives.Length == 0)
@@ -55,7 +55,7 @@ namespace KatlaSport.Services.HiveManagement
         }
 
         /// <inheritdoc/>
-        public async Task<Hive> CreateHive(UpdateHiveRequest createRequest)
+        public async Task<Hive> CreateHiveAsync(UpdateHiveRequest createRequest)
         {
             var dbHives = await _context.Hives.Where(h => h.Code == createRequest.Code).ToArrayAsync();
             if (dbHives.Length > 0)
@@ -74,7 +74,7 @@ namespace KatlaSport.Services.HiveManagement
         }
 
         /// <inheritdoc/>
-        public async Task<Hive> UpdateHive(int hiveId, UpdateHiveRequest updateRequest)
+        public async Task<Hive> UpdateHiveAsync(int hiveId, UpdateHiveRequest updateRequest)
         {
             var dbHives = await _context.Hives.Where(p => p.Code == updateRequest.Code && p.Id != hiveId).ToArrayAsync();
             if (dbHives.Length > 0)
@@ -99,7 +99,7 @@ namespace KatlaSport.Services.HiveManagement
         }
 
         /// <inheritdoc/>
-        public async Task DeleteHive(int hiveId)
+        public async Task DeleteHiveAsync(int hiveId)
         {
             var dbHives = await _context.Hives.Where(p => p.Id == hiveId).ToArrayAsync();
             if (dbHives.Length == 0)
--- KatlaSport.WebApi/Controllers/HivesController.cs
+++ KatlaSport.WebApi/Controllers/HivesController.cs
@@ -30,21 +30,21 @@ namespace KatlaSport.WebApi.Controllers
         [HttpGet]
         [Route("")]
         [SwaggerResponse(HttpStatusCode.OK, Description = "Returns a list of hives.", Type = typeof(HiveListItem[]))]
-        [SwaggerResponse(HttpStatusCode.InternalServerError)]
+        [SwaggerResponse(HttpStatusCode.InternalServerError, Description = "The server encountered an unhandled situation.")]
         public async Task<IHttpActionResult> GetHives()
         {
-            var hives = await _hiveService.GetHives();
+            var hives = await _hiveService.GetHivesAsync();
             return Ok(hives);
         }
 
         [HttpGet]
         [Route("{hiveId:int:min(1)}")]
         [SwaggerResponse(HttpStatusCode.OK, Description = "Returns a hive.", Type = typeof(Hive))]
-        [SwaggerResponse(HttpStatusCode.NotFound)]
-        [SwaggerResponse(HttpStatusCode.InternalServerError)]
+        [SwaggerResponse(HttpStatusCode.NotFound, Description = "The server can not find the requested resource.")]
+        [SwaggerResponse(HttpStatusCode.InternalServerError, Description = "The server encountered an unhandled situation.")]
         public async Task<IHttpActionResult> GetHive(int hiveId)
         {
-            var hive = await _hiveService.GetHive(hiveId);
+            var hive = await _hiveService.GetHiveAsync(hiveId);
             return Ok(hive);
         }
 
@@ -52,7 +52,7 @@ namespace KatlaSport.WebApi.Controllers
         [Route("{hiveId:int:min(1)}/sections")]
         [SwaggerResponse(HttpStatusCode.OK, Description = "Returns a list of hive sections for specified hive.", Type = typeof(HiveSectionListItem))]
         [SwaggerResponse(HttpStatusCode.NotFound)]
-        [SwaggerResponse(HttpStatusCode.InternalServerError)]
+        [SwaggerResponse(HttpStatusCode.InternalServerError, Description = "The server encountered an unhandled situation.")]
         public async Task<IHttpActionResult> GetHiveSections(int hiveId)
         {
             var hive = await _hiveSectionService.GetHiveSectionsAsync(hiveId);
@@ -62,12 +62,61 @@ namespace KatlaSport.WebApi.Controllers
         [HttpPut]
         [Route("{hiveId:int:min(1)}/status/{deletedStatus:bool}")]
         [SwaggerResponse(HttpStatusCode.NoContent, Description = "Sets deleted status for an existed hive.")]
-        [SwaggerResponse(HttpStatusCode.NotFound)]
-        [SwaggerResponse(HttpStatusCode.InternalServerError)]
+        [SwaggerResponse(HttpStatusCode.NotFound, Description = "The server can not find the requested resource.")]
+        [SwaggerResponse(HttpStatusCode.InternalServerError, Description = "The server encountered an unhandled situation.")]
         public async Task<IHttpActionResult> SetStatus([FromUri] int hiveId, [FromUri] bool deletedStatus)
         {
             await _hiveService.SetStatusAsync(hiveId, deletedStatus);
             return ResponseMessage(Request.CreateResponse(HttpStatusCode.NoContent));
+        }
+
+        [HttpPost]
+        [Route("")]
+        [SwaggerResponse(HttpStatusCode.Created, Description = "Creates a new hive.")]
+        [SwaggerResponse(HttpStatusCode.BadRequest, Description = "Unknown query syntax for the server.")]
+        [SwaggerResponse(HttpStatusCode.Conflict, Description = "Request conflict with current state of server.")]
+        [SwaggerResponse(HttpStatusCode.InternalServerError, Description = "The server encountered an unhandled situation.")]
+        public async Task<IHttpActionResult> AddHive([FromBody] UpdateHiveRequest createRequest)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+                
+            var hive = await _hiveService.CreateHiveAsync(createRequest);
+            var location = string.Format("/api/hives/{0}", hive.Id);
+            return Created<Hive>(location, hive);
+
+        }
+
+        [HttpPut]
+        [Route("{id:int:min(1)}")]
+        [SwaggerResponse(HttpStatusCode.OK, Description = "Updated hive by id.")]
+        [SwaggerResponse(HttpStatusCode.BadRequest, Description = "Unknown query syntax for the server.")]
+        [SwaggerResponse(HttpStatusCode.Conflict, Description = "Request conflict with current state of server.")]
+        [SwaggerResponse(HttpStatusCode.InternalServerError, Description = "The server encountered an unhandled situation.")]
+        public async Task<IHttpActionResult> UpdateHive([FromUri] int id, [FromBody] UpdateHiveRequest updateRequest)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _hiveService.UpdateHiveAsync(id, updateRequest);
+            return ResponseMessage(Request.CreateResponse(HttpStatusCode.OK));
+        }
+
+        [HttpDelete]
+        [Route("{id:int:min(1)}")]
+        [SwaggerResponse(HttpStatusCode.OK, Description = "Deleted a hive by id.")]
+        [SwaggerResponse(HttpStatusCode.NoContent, Description = "No content to answer the request.")]
+        [SwaggerResponse(HttpStatusCode.BadRequest, Description = "Unknown query syntax for the server.")]
+        [SwaggerResponse(HttpStatusCode.Conflict, Description = "Request conflict with current state of server.")]
+        [SwaggerResponse(HttpStatusCode.InternalServerError, Description = "The server encountered an unhandled situation.")]
+        public async Task<IHttpActionResult> DeleteHive([FromUri] int id)
+        {
+            await _hiveService.DeleteHiveAsync(id); 
+            return ResponseMessage(Request.CreateResponse(HttpStatusCode.OK));
         }
     }
 }
